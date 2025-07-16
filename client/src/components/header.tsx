ba36703b32223799e055ab62ec68f336de0b3852
Rumon360 <hmk.rumon@gmail.com>
2025-07-17 01:00:17 +0600

    fix: header style

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="sticky top-0 border-b py-4 backdrop-blur-sm">
-      <div className="container mx-auto flex justify-between px-4">
+    <header className="sticky top-0 h-14 border-b backdrop-blur-sm">
+      <div className="container mx-auto flex h-full w-full items-center justify-between px-4">
         <div className="text-foreground text-lg font-semibold">
           <span>
             Rag<span className="text-blue-500">Chat</span>App
