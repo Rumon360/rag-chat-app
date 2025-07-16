91141a097c1fb1c0860a96feac34ca709af3cac6
Rumon360 <hmk.rumon@gmail.com>
2025-07-17 01:06:38 +0600

    update: theme and auth layout

--- client/src/app/(auth)/layout.tsx
+++ client/src/app/(auth)/layout.tsx
@@ -2,16 +2,36 @@ import "@/styles/globals.css";
 
 import { type Metadata } from "next";
 
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import { Geist } from "next/font/google";
+
 export const metadata: Metadata = {
   title: "Auth - RAG Chat App",
 };
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
 
 export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      {children}
-    </div>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" className={`${geist.variable}`}>
+        <body>
+          <main className="flex h-screen w-full items-center justify-center">
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
